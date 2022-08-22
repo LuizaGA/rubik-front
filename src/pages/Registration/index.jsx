7c0c3198745abd215baee8c0380087de2f4bd894
Luiza Gadelha <luizagadelha@unifor.com>
2022-08-22 14:02:22 -0300

    fix: pag cadastro completa

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,22 +1,21 @@
 import { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import LinkWrapper from '../../components/LinkWrapper'
-import {ContainerGeral, ContainerLogo, NomeSite, Texto, Label, Input} from './styles'
+import { ContainerGeral, ContainerLogo,Logo,  NomeSite, Texto, Label, Input, Legenda, LabelMenor, ContainerGenero, TextoObs, Span, InputSubmit } from './styles'
 
 function Registration() {
   const formRef = useRef(null);
+  const [email, setEmail] = useState('');
+  const [confEmail, setConfEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [birthday, setBirthday] = useState('');
   const [genero, setGenero] = useState('');
   const [marketing, setMarketing] = useState(false);
   const [compartilhar, setCompartilhar] = useState(false);
   const [termos, setTermos] = useState(false);
-
-  function handleRegistration(e){
-    e.preventDefault();
-
-    console.log('cadastrado');
-    console.log(genero);
-    formRef.current.reset();
-  }
-
+  
   const handleGenero = (e) => {
     setGenero(e.target.value);
   }
@@ -24,55 +23,113 @@ function Registration() {
   const handleMarketing = (e) => {
     setMarketing(!marketing);
   }
-
+  
   const handleCompartilhar = (e) => {
     setCompartilhar(!compartilhar);
   }
-
+  
   const handleTermos= (e) => {
     setTermos(!termos);
+  }
+
+  const notifyWarn = (text) => toast.warn(text, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
+  const notifySucess = () => toast.success('Cadastro realizado com sucesso', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
+  function handleRegistration(e){
+    e.preventDefault();
+    if (!termos) {
+      notifyWarn('Por favor, aceite os termos de uso');
+    } else if (email != confEmail) {
+      notifyWarn('Por favor, coloque o mesmo email nos dois campos');
+    } else if( !genero) {
+      notifyWarn('Por favor, preencha todos os campos do formulário');
+    } else {
+      let dados = {
+        email: email,
+        senha: password,
+        nome: name,
+        aniversario: birthday,
+        genero: genero,
+        aceitaMarketing: marketing,
+        aceitaCompartilhar: compartilhar,
+        aceitaTermos: termos,
+      }
+      notifySucess();
+      console.log(dados);
+      formRef.current.reset();
+      setEmail('');
+      setConfEmail('');
+      setName('');
+      setPassword('');
+      setBirthday('');
+      setGenero('');
+      setMarketing(false);
+      setCompartilhar(false);
+      setTermos(false);
+    }
   }
 
   return (
     <ContainerGeral>
       <ContainerLogo>
-      <img src="logo_black.png" alt="logo" />
+      <Logo src="logo_black.png" alt="logo" />
       <NomeSite>Rubik</NomeSite>
       </ContainerLogo>
       <Texto>Inscreva-se grátis e comece a curtir.</Texto>
       <form onSubmit={handleRegistration} ref={formRef}>
         <Label htmlFor="email">Qual o seu email?</Label>
-        <Input id="email" type="email" placeholder="Insira seu email."/>
+        <Input id="email" type="email" placeholder="Insira seu email." value={email} onChange={(e) => setEmail(e.target.value)} required/>
         <Label htmlFor="confirm">Confirme seu email</Label>
-        <Input id="confirm" type="email" placeholder="Insira seu email novamente."/>
+        <Input id="confirm" type="email" placeholder="Insira seu email novamente." value={confEmail} onChange={(e) => setConfEmail(e.target.value)} required/>
         <Label htmlFor="password">Crie uma senha</Label>
-        <Input id='password' type="password" placeholder="Crie uma senha."/>
+        <Input id='password' type="password" placeholder="Crie uma senha." value={password} onChange={(e) => setPassword(e.target.value)} required/>
         <Label htmlFor="name">Como devemos chamar você?</Label>
-        <Input id='name' type="text" placeholder="Insira um nome de perfil."/>
-        <legend>Qual é o seu gênero?</legend>  
-          <label>
+        <Input id='name' type="text" placeholder="Insira um nome de perfil." value={name} onChange={(e) => setName(e.target.value)} required/>
+        <Label htmlFor="date">Qual a sua data de nascimento?</Label>
+        <Input id="date" type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)} required/>
+        <Legenda>Qual é o seu gênero?</Legenda>  
+        <ContainerGenero>
+          <LabelMenor>
             <input type="radio" value="masculino" name="gender" checked={genero === 'masculino'} onChange={handleGenero} /> Masculino
-          </label>
-          <label>
+          </LabelMenor>
+          <LabelMenor>
             <input type="radio" value="feminino" name="gender" checked={genero === 'feminino'} onChange={handleGenero} /> Feminino
-          </label>
-          <label>
+          </LabelMenor>
+          <LabelMenor>
             <input type="radio" value="naoBinario" name="gender" checked={genero === 'naoBinario'} onChange={handleGenero} /> Não binário
-          </label>
-          <br />
-          <label>
-            <input type="checkbox" value="marketing" checked={marketing} onChange={handleMarketing}/>Não quero receber mensagens de marketing do Spotify
-          </label>
-          <label>
-            <input type="checkbox" value="compartilhar" checked={compartilhar} onChange={handleCompartilhar}/>Compartilhar meus dados cadastrais com os provedores de conteúdo do Spotify para fins de marketing.
-          </label>
-          <label>
-            <input type="checkbox" value="termos" checked={termos} onChange={handleTermos}/>Eu concordo com os Termos e Condições de Uso do Spotify.
-          </label>
-          <p>Para saber mais sobre como o Spotify coleta, utiliza, compartilha e protege seus dados pessoais, leia a Política de Privacidade do Spotify.</p>
-        <input type="submit" value="Inscrever-se"/>
+          </LabelMenor>
+        </ContainerGenero>
+          <LabelMenor style={{display: 'block', marginTop:'1.5rem'}}>
+            <input type="checkbox" value="marketing" checked={marketing} onChange={handleMarketing}/>  Não quero receber mensagens de marketing do Rubik
+          </LabelMenor>
+          <LabelMenor style={{display: 'block', marginTop:'1.5rem'}}>
+            <input type="checkbox" value="compartilhar" checked={compartilhar} onChange={handleCompartilhar}/>  Compartilhar meus dados cadastrais com os provedores de conteúdo do Rubik para fins de marketing.
+          </LabelMenor>
+          <LabelMenor style={{display: 'block', marginTop:'1.5rem'}}>
+            <input type="checkbox" value="termos" checked={termos} onChange={handleTermos}/>  Eu concordo com os Termos e Condições de Uso do Rubik.
+          </LabelMenor>
+          <TextoObs>Para saber mais sobre como o Rubik coleta, utiliza, compartilha e protege seus dados pessoais, leia a Política de Privacidade do Rubik.</TextoObs>
+        <InputSubmit type="submit" value="Inscrever-se"/>
+        <ToastContainer />
       </form>
-      <p>Já tem uma conta? <LinkWrapper to="/login"><p style={{color: 'black'}}>Faça login.</p></LinkWrapper></p>
+      <TextoObs style={{fontSize:'1rem'}}>Já tem uma conta? <LinkWrapper to="/login"><Span>Faça login.</Span></LinkWrapper></TextoObs>
     </ContainerGeral>
   )
 }
--- src/pages/Registration/styles.js
+++ src/pages/Registration/styles.js
@@ -3,6 +3,18 @@ import styled from 'styled-components'
 export const ContainerGeral = styled.div`
   width: 28rem;
   margin: 3rem auto;
+
+  @media only screen and (max-width: 520px) {
+    width: 25rem;
+  }
+
+  @media only screen and (max-width: 426px) {
+    width: 21rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    width: 18rem;
+  }
 `
 
 export const ContainerLogo = styled.div`
@@ -11,12 +23,29 @@ export const ContainerLogo = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  @media only screen and (max-width: 426px) {
+    height: 4rem;
+  }
+`
+
+export const Logo = styled.img`
+  height: 5rem;
+
+  @media only screen and (max-width: 426px) {
+    height: 3rem;
+  }
 `
 
 export const NomeSite = styled.h3`
   font-size: 3rem;
   margin-left: 1rem;
   font-family: 'Silkscreen', cursive;
+
+  @media only screen and (max-width: 426px) {
+    font-size: 2rem;
+    margin-left: 0.5rem;
+  }
 `
 
 export const Texto = styled.h2`
@@ -24,12 +53,23 @@ export const Texto = styled.h2`
   margin: 2rem 0;
   font-weight: 700;
   text-align: center;
+
+  @media only screen and (max-width: 426px) {
+    font-size: 1.5rem;
+    margin: 1rem 0;
+  }
 `
 
 export const Label = styled.label`
-  font-size: 1rem;
+  font-size: 0.8rem;
+  font-weight: 700;
   display: block;
-  margin: 0.5rem 0 0.1rem 0;
+  margin: 1.5rem 0 0.5rem 0;
+
+  ::placeholder {
+    font-size: 0.6rem;
+    font-family: 'Inter', sans-serif;
+  }
 `
 
 export const Input = styled.input`
@@ -37,4 +77,56 @@ export const Input = styled.input`
   width: 100%;
   border: 1px solid black;
   border-radius: 4px;
+`
+
+export const Legenda = styled.legend`
+  font-size: 0.8rem;
+  font-weight: 700;
+  display: block;
+  margin: 1.5rem 0 0.5rem 0;
+`
+
+export const LabelMenor = styled.label`
+  font-size: 0.8rem;
+  margin-right: 1rem;
+`
+
+export const ContainerGenero = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const TextoObs = styled.p`
+  font-size: 0.6rem;
+  margin: 1.5rem 0;
+  text-align: center;
+`
+
+export const Span = styled.span`
+  color: #286b45;
+  text-decoration: underline;
+`
+
+export const InputSubmit = styled.input`
+  height: 3.5rem;
+  width: 11rem;
+  border-radius: 31rem;
+  background-color: #286b45;
+  color: white;
+  font-size: 1rem;
+  font-weight: 700;
+  margin-left: 8rem;
+
+  @media only screen and (max-width: 520px) {
+    margin-left: 6.5rem;
+  }
+
+  @media only screen and (max-width: 426px) {
+    margin-left: 4.9rem;
+  }
+
+  @media only screen and (max-width: 350px) {
+    margin-left: 3.5rem;
+  }
 `
